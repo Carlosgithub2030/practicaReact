fd3e4ca806f258a74d1eb2d41e1eef30b47fe5c0
medina juan carlos <juan_carlos_medina2030@hotmail.com>
2022-11-17 23:48:52 -0300

    rama dev

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+  return (
+    <>
+     <h1 id="titulo">Hola mundo</h1>
+     <p id="descripcion">esta es una pagina de prueba react</p>
+    </>
+  );
+}
+
+export default App;
